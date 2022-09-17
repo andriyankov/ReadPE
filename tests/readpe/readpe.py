ca8f102d0fe53a72e58824bd7b7b0dbc7e87c7bd
Dmitry Andriyankov <d.andriyankov@gmail.com>
2022-09-17 13:35:28 +0300

    #5 : Refactoring to readpe python module

--- tests/readpe/readpe.py
+++ tests/readpe/readpe.py
@@ -5,6 +5,11 @@ from subprocess import run, PIPE
 from enum import IntEnum, unique
 
 
+_APP_PATH = 'readpe.exe'
+_APP_NAME_PATTERN = 'readpe.*.exe'
+_DEFAULT_APP_ARGS = '--nologo --raw --export --imports --tls --detailed'
+
+
 @unique
 class ExitCode(IntEnum):
     SUCCESS = 0
@@ -17,15 +22,11 @@ class ExitCode(IntEnum):
     NOT_IMPLEMENTED = 7
     CONFIGURE_TOOL = 8
 
-_APP_PATH = 'readpe.exe'
-_APP_NAME_PATTERN = 'readpe.*.exe'
-_DEFAULT_APP_ARGS = '--nologo --raw --export --imports --tls --detailed'
-
 
 class ReadpeException(Exception):
-    def __init__(self, app_result):
+    def __init__(self, retcode, stdout, stderr):
         super().__init__()
-        self.__app_result = app_result
+        self.__app_result = dict(retcode=retcode, stdout=stdout, stderr=stderr)
 
     def __str__(self):
         return f'Exit code: {self.error_code}'
@@ -52,6 +53,19 @@ class ScriptException(Exception):
         return 'ReadPE Script error. ' + self.__errtext
 
 
+class Result:
+    def __init__(self, retcode, stdout, stderr):
+        if retcode != ExitCode.SUCCESS:
+            raise ReadpeException(retcode, stdout, stderr)
+        if not stdout:
+            raise ScriptException('STDOUT is empty')
+        if not stderr:
+            raise ScriptException('STDERR is empty')
+        self.retcode = retcode
+        self.stdout = stdout
+        self.stderr = stderr
+
+
 class ReadPE:
     def __init__(self, apppath=_APP_PATH, args=_DEFAULT_APP_ARGS):
         self.__check_apppath(apppath)
@@ -68,42 +82,24 @@ class ReadPE:
             err_text = fmt % (_APP_PATH, cur_app_name)
             raise ScriptException(err_text)
 
-    def __check_srcfile(self, srcfile):
+    def run(self, srcfile):
         if not os.path.exists(srcfile):
             raise ScriptException('Source file not exists')
-
-    def __check_results(self):
-        if self.__app_result['retcode'] != ExitCode.SUCCESS:
-            raise ReadpeException(self.__app_result)
-        if not self.__app_result['stdout']:
-            raise ScriptException('STDOUT is empty')
-        if not self.__app_result['stderr']:
-            raise ScriptException('STDERR is empty')
-
-    def __run_process(self, args):
-        proc = run(args, stdout=PIPE, stderr=PIPE)
-        stdout_bin = proc.stdout
-        stderr_bin = proc.stderr
-        self.__app_result['retcode'] = proc.returncode
-        self.__app_result['stdout'] = stdout_bin.decode('ascii')
-        self.__app_result['stderr'] = stderr_bin.decode('ascii')
-        self.__check_results()
-
-    def run(self, srcfile):
-        self.__check_srcfile(srcfile)
         try:
             cmdline = f'{self.__apppath} {self.__args} "{srcfile}"'
-            self.__run_process(cmdline)
+            proc = run(cmdline, stdout=PIPE, stderr=PIPE)
+            self.__app_result = Result(proc.returncode,
+                proc.stdout.decode('ascii'), proc.stderr.decode('ascii'))
         except UnicodeDecodeError as e:
             raise ScriptException('Connot decode ReadPE stdout or stderr')
 
     @property
     def stdout(self):
-        return self.__app_result['stdout']
+        return self.__app_result.stdout
 
     @property
     def stderr(self):
-        return self.__app_result['stderr']
+        return self.__app_result.stderr
 
 
 def call_readpe_tool(filename, apppath='readpe.exe', args=_DEFAULT_APP_ARGS):
